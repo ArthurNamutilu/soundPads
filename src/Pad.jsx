874b6b39601aad65c5b10ec5d1123d36d61c4dd0
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-12-30 07:50:17 +0300

    local state

--- src/Pad.jsx
+++ src/Pad.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 export default function Pad(props) {
+  const [on, setOn] = useState(props.on);
+  function toggle() {
+    setOn((prevOn) => !prevOn);
+  }
+
   return (
     <button
-      className={props.on ? "on" : undefined}
+      className={on ? "on" : undefined}
       style={{ backgroundColor: props.color }}
+      onClick={toggle}
     ></button>
   );
 }
