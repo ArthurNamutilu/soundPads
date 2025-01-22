245caeb0170eabd73f4db1825c4cc9543fba0503
ArthurNamutilu <arthurggnamutilu@gmail.com>
2025-01-23 00:07:23 +0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
